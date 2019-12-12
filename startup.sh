0bc123336d8f7b72c89c9ae1d8f1eb2aedacafce
Kirmark <37423353+Kirmark@users.noreply.github.com>
2019-12-12 22:37:13 +0300

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# chmod +x startup.sh - выдать разрешение на выполнение
+
+# разрешения для запуска связанных файлов
+
+chmod +x connect.sh
+
+pwd
+whoami
+
+## Настрйока проекта
+
+sudo apt-get update
+sudo apt-get install unzip
+
+yes Y | sudo rm -r app
+mkdir app
+
+## Докер
+
+if [ "$1" == "-n" ]; then
+  yes Y | sudo apt-get remove docker docker-engine docker.io
+  yes Y | sudo apt install docker.io
+fi
+
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+sudo docker build -t jupyter_datascience ./
+
+sudo docker kill jupyter_datascience_instance
+sudo docker rm jupyter_datascience_instance
+
+sudo docker run -it -p 8888:8888 --name jupyter_datascience_instance -v $(pwd):/tf/server jupyter_datascience:latest
